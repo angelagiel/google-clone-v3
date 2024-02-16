cd62d918893bc241016c4b9b757aec5d1138f505
limlingan <limlinganangela@gmail.com>
2024-02-16 10:40:00 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+// import Search from "./Search"
+import AppsIcon from '@mui/icons-material/Apps';
+import { Avatar } from '@mui/material';
+
+
+function Home() {
+  return <div className="home">
+    <div className="home__header">
+        <div className="home__headerLeft">
+            <Link to='/about'>About</Link>
+            <Link to='/store'>Store</Link>
+        </div>
+        <div className="home__headerRight">
+            <Link to='/gmail'>Gmail</Link>
+            <Link to='/gmail'>Images</Link>
+            <AppsIcon />
+            <Avatar />
+        </div>
+        
+    </div>
+
+    <div className="home__body">
+        
+    </div>
+
+    </div>;
+}
+
+export default Home;
---
+++ src/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Search() {
+  return <div className="search">
+    <h1>SEARCH NATO</h1>
+  </div>;
+}
+
+export default Search;
